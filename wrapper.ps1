25ec7ec0d528fd37e26ca836fca580cb7bbfb6e1
badgeart23 <admin@badgeart.de>
2025-05-06 11:04:49 +0200

    wrapper.ps1

--- wrapper.ps1
+++ wrapper.ps1
@@ -1,4 +1,14 @@
+# TLS 1.2 erzwingen, damit der Download auch auf alten Systemen funktioniert
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Batch-Datei herunterladen
 $batchContent = Invoke-RestMethod 'https://raw.githubusercontent.com/badgeart23/office-Aktivierungsassistent/main/office_Aktivierungshilfe.cmd'
+
+# Temporäre Datei anlegen
 $tempFile = "$env:TEMP\office_Aktivierungshilfe.cmd"
+
+# Inhalt speichern
 $batchContent | Out-File -FilePath $tempFile -Encoding ASCII
+
+# Batch-Datei starten
 Start-Process cmd.exe "/c $tempFile"
